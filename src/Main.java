0fdfe01932a7a1716caa827dc42c543e6c94a604
ace-krypton <ramizna@code.edu.az>
2022-03-17 23:09:46 +0100

    `TODO` added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+      * TODO In the main method, create a seaport with at least five different types of container ships loaded with different containers.
+      *  Additionally, several containers of different types should be placed in the warehouse
+      **/
+
+    public static void main(String[] args) {
+        
+    }
+}
