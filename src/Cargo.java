4cc5644d146c85ab4d799bb030125f3014c6122c
ace-krypton <ramizna@code.edu.az>
2022-03-18 21:01:47 +0100

    Added `TODO` items

---
+++ src/Cargo.java
@@ -0,0 +1,10 @@
+public class Cargo {
+    /*
+     * TODO
+     *      • Explosive Cargo
+     *      • Toxic Cargo
+     *      • Liquid Cargo
+     *      • Toxic Powdery Cargo
+     *      • Toxic Liquid Cargo
+     */
+}
---
+++ src/Containers.java
@@ -0,0 +1,16 @@
+public class Containers {
+    /*
+    * TODO
+    *  • standard container,
+    *  • heavy container, which is a type of standard container,
+    *  • a refrigerated container, which is a type of heavy container, requiring connection to the
+    *      ship’s electrical network,
+    *  • a container for liquids cargo, which is a type of standard container,
+    *  • a container for explosives cargo, which is a type of standard container,
+    *  • a container for toxic cargo, which is a type of heavy container, which is available in two
+    *      versions: a container for toxic powdery cargo and a container for toxic liquid cargo, which
+    *      is not only a kind of heavy container, but also has the features of a container for liquids
+    *      cargo.
+    */
+
+}
