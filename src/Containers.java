7f779fe6b2d83917fbfccacfb6a625a2ef333589
ace-krypton <ramizna@code.edu.az>
2022-04-06 16:42:48 +0200

    Small changes

--- src/Containers.java
+++ src/Containers.java
@@ -68,7 +68,7 @@ public class Containers {
         System.out.println("+-----------------------------------------+");
         endTime = System.currentTimeMillis();
         long timeElapsed = endTime - startTime;
-        if (timeElapsed >= 70000) {
+        if (timeElapsed >= 10000) {
             for (StandardContainer containers : standardContainers) {
                 containers.setSender(null);
                 containers.setCertificateInfo(null);
